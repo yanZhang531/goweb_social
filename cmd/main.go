4a1e4cd1699ae2db67252d6f4e07322a578597c2
yanZhang531 <869935823@qq.com>
2023-05-25 16:07:21 +0800

    博客后端整体实现，待优化

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"github.com/pzlymformeet/social/pkg/routers"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	routers.RegisterAllRouter(mux)
+	http.ListenAndServe(":8010", mux)
+
+}
